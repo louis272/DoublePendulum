76ca91a7c4b2696b5eeff230ca3703011cfb7ca9
Louis Schanen <louis.schanen@gmail.com>
2026-01-08 16:01:49 +0100

    Add functions to create DoublePendule and real-world pendulum instances

--- src/system.jl
+++ src/system.jl
@@ -9,3 +9,34 @@ mutable struct DoublePendule
     g::Float64             # Gravité [m/s^2]
     state::Vector{Float64} # [θ1, θ2, ω1, ω2]
 end
+
+"""
+    Crée et retourne un objet DoublePendule.
+"""
+function create_pendulum(l1, l2, m1, m2, g, state_init)
+    p1 = Pendule(l1, m1)
+    p2 = Pendule(l2, m2)
+
+    return DoublePendule(p1, p2, g, state_init)
+end
+
+"""
+    Crée un pendule double similaire à celui de la vidéo.
+"""
+function create_real_pendulum()
+    l1 = 91.74e-2 # Longueur tige 1 (m)
+    l2 = 69.33e-2 # Longueur tige 2 (m)
+
+    m1 = 30.00e-2 # Masse aléatoire pendule 1 (kg)
+    m2 = 2.00e-2  # Masse aléatoire pendule 2 (kg)
+
+    g = 9.81 # Accélération gravitationnelle (m/s²)
+
+    theta1_0 = π/2
+    theta2_0 = π/2
+    omega1_0 = 0.0
+    omega2_0 = 0.0
+    initial_state = [theta1_0, theta2_0, omega1_0, omega2_0]
+
+    return create_pendulum(l1, l2, m1, m2, g, initial_state)
+end
